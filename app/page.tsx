f0cea7307f0db37625cd2f769ce2a53bbb20bffe
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-27 18:24:54 +0900

    callback

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,35 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { showSuccessToast } from "@/hooks/useNotification";
+import authOptions from "./api/auth/[...nextauth]/authOptions";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  function handleClick() {
-    showSuccessToast("https://github.com/tnkshuuhei");
-  }
+  // useEffect(() => {
+  //   const hash = window.location.hash;
+  //   let token: any = window.localStorage.getItem("token");
+  //   if (!token && hash) {
+  //     token = hash
+  //       .substring(1)
+  //       ?.split("&")
+  //       .find((item) => item.includes("access_token"))
+  //       ?.split("=")[1];
+  //     window.localStorage.setItem("token", token);
+  //     setToken(token);
+  //     console.log("token: ", token);
+  //   }
+  // }, []);
+  // const logout = () => {
+  //   window.localStorage.removeItem("token");
+  //   setToken(null);
+  // };
+  const { data: session, status } = useSession();
+  if (status === "loading") return <div>Loading...</div>;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button onClick={handleClick}>Click me</Button>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      {session ? <Button>Logout</Button> : <Button>Login with Spotify</Button>}
     </main>
   );
 }
