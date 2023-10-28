86944e64234c0b190d5795bbf52995276d014392
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-28 20:46:15 +0900

    fixed authentication

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -1,34 +1,37 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-const scopes = [
-  "user-read-email",
-  "playlist-read-private",
-  "playlist-read-collaborative",
-  "user-read-currently-playing",
-  "user-modify-playback-state",
-].join(",");
-
-const params = {
-  scope: scopes,
-};
 const auth_url = "https://accounts.spotify.com/authorize";
 const response_type = "token";
 const redirect_uri = "http://localhost:3000";
-const clientId = process.env.SPITIFY_CLIENT_ID as string;
+const clientId = process.env.SPOTIFY_CLIENT_ID as string;
 const LOGIN_URL = `${auth_url}?client_id=${clientId}&redirect_uri=${redirect_uri}&response_type=${response_type}`;
 
 const authOptions: NextAuthOptions = {
   providers: [
     SpotifyProvider({
-      clientId: process.env.SPITIFY_CLIENT_ID as string,
-      clientSecret: process.env.SPITIFY_CLIENT_SECRET as string,
-      authorization: LOGIN_URL,
+      clientId: clientId,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
     }),
   ],
   secret: process.env.JWT_SECRET,
-
-  callbacks: {},
+  // callbacks: {
+  //   async jwt({ token, account }) {
+  //     // Persist the OAuth access_token to the token right after signin
+  //     if (account) {
+  //       token.accessToken = account.access_token;
+  //       token.refreshToken = account.refresh_token;
+  //       token.accessTokenExpires = account.expires_at;
+  //       return token;
+  //     }
+  //     return token;
+  //   },
+  //   async session({ session, token, user }) {
+  //     // Send properties to the client, like an access_token from a provider.
+  //     session.accessToken = token.accessToken;
+  //     return session;
+  //   },
+  // },
 };
 
 export default authOptions;
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,18 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import authOptions from "./api/auth/[...nextauth]/authOptions";
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth";
-import { useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  // useEffect(() => {
-  //   const hash = window.location.hash;
-  //   let token: any = window.localStorage.getItem("token");
-  //   if (!token && hash) {
-  //     token = hash
-  //       .substring(1)
-  //       ?.split("&")
-  //       .find((item) => item.includes("access_token"))
-  //       ?.split("=")[1];
-  //     window.localStorage.setItem("token", token);
-  //     setToken(token);
-  //     console.log("token: ", token);
-  //   }
-  // }, []);
-  // const logout = () => {
-  //   window.localStorage.removeItem("token");
-  //   setToken(null);
-  // };
-  const { data: session, status } = useSession();
-  if (status === "loading") return <div>Loading...</div>;
+  const session = useSession();
+  console.log("session: ", session);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      {session ? <Button>Logout</Button> : <Button>Login with Spotify</Button>}
+      {session.status === "authenticated" ? (
+        <Button onClick={() => signOut()}>Logout</Button>
+      ) : (
+        <Button onClick={() => signIn()}>Login with Spotify</Button>
+      )}
     </main>
   );
 }
